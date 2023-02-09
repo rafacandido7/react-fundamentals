b16997f1fedea2e8af531b618edea59a9a73470c
Rafael Candido <rafacandido0708@gmail.com>
2023-02-09 14:41:01 -0300

    gerando o bundle com o webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
-require()
-module.exports = {
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+module.exports = {
+  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js'
+  },
+  mode: 'development',
+  plugins: [
+    new HTMLWebpackPlugin ({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new CleanWebpackPlugin()
+  ]
 }
