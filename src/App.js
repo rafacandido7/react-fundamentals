9a333f10ab418613b9b1113d0fb2f482bd29faec
Rafael Candido <rafacandido0708@gmail.com>
2023-02-09 21:37:16 -0300

    Renderização de listas

--- src/App.js
+++ src/App.js
@@ -2,34 +2,31 @@ import React from 'react'
 import Header from './components/Header'
 import Post from './components/Post'
 
+const posts = [
+  { title: 'Title#01', subtitle: 'Subtitle#01', likes: 50 },
+  { title: 'Title#02', subtitle: 'Subtitle#02', likes: 12 },
+  { title: 'Title#03', subtitle: 'Subtitle#03', likes: 43 },
+]
+
 function App() {
   return (
     <>
       <Header >
         <h2>Posts da semanaa</h2>
       </Header>
+
       <hr />
-      <Post
-        likes={10}
-        post={{
-          title: 'Titulo da notícia 01',
-          subtitle: 'Subtitulo da notícia 01',
-        }}
-      />
-      <Post
-        likes={10}
-        post={{
-          title: 'Titulo da notícia 02',
-          subtitle: 'Subtitulo da notícia 02',
-        }}
-      />
-      <Post
-        likes={10}
-        post={{
-          title: 'Titulo da notícia 03',
-          subtitle: 'Subtitulo da notícia 03',
-        }}
-      />
+
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle
+          }}
+          likes={post.likes}
+        />
+      ))}
     </>
   )
 }
